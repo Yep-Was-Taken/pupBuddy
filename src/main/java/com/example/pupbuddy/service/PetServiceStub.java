e5c8e2372bcd53fc8b3392d45c3c039770665014
Vedant Dharmesh Amin <54548039+aminvd@users.noreply.github.com>
2022-02-27 16:45:45 -0500

    Create PetServiceStub.java
    There for no service package present so I created and add petServiceStub

---
+++ src/main/java/com/example/pupbuddy/service/PetServiceStub.java
@@ -0,0 +1,8 @@
+package com.example.pupbuddy.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetServiceStub{
+    // public class PetServiceStub will implement the IPetService
+
+}
