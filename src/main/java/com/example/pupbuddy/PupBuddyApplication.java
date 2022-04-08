faea7fadd463bf7b5b280a3f6985f3d2e9dbb93f
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-04-08 10:08:34 -0400

    Added code to initialize firebase in PupBuddyApplication - file path to credentials has been omitted for security

--- src/main/java/com/example/pupbuddy/PupBuddyApplication.java
+++ src/main/java/com/example/pupbuddy/PupBuddyApplication.java
@@ -1,12 +1,32 @@
 package com.example.pupbuddy;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class PupBuddyApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = PupBuddyApplication.class.getClassLoader();
+
+        FileInputStream serviceAccount = new FileInputStream("${firebase.credential.path}");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+
+        FirebaseApp.initializeApp(options);
+
         SpringApplication.run(PupBuddyApplication.class, args);
     }
 
