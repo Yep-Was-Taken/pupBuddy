6da37021195eaa80c93ff9d03ddbc1ed2f4a6ab6
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-04-08 13:29:19 -0400

    Updated methods in LoginCRUDService, updated method signatures in ILoginCRUDService

--- src/main/java/com/example/pupbuddy/service/ILoginCRUDService.java
+++ src/main/java/com/example/pupbuddy/service/ILoginCRUDService.java
@@ -5,8 +5,8 @@ import com.example.pupbuddy.dto.Login;
 import java.util.concurrent.ExecutionException;
 
 public interface ILoginCRUDService {
-    public String createLogin(Login login) throws ExecutionException, InterruptedException ;
-    public Login getLogin(int loginId);
-    public String updateLogin(Login login);
-    public String deleteLogin(int loginId);
+    public String createLogin(Login login) throws ExecutionException, InterruptedException;
+    public Login getLogin(String loginId) throws ExecutionException, InterruptedException;
+    public String updateLogin(Login login) throws ExecutionException, InterruptedException;
+    public String deleteLogin(String loginId);
 }
--- src/main/java/com/example/pupbuddy/service/LoginCRUDService.java
+++ src/main/java/com/example/pupbuddy/service/LoginCRUDService.java
@@ -3,6 +3,7 @@ package com.example.pupbuddy.service;
 import com.example.pupbuddy.dto.Login;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
@@ -12,26 +13,36 @@ import java.util.concurrent.ExecutionException;
 
 @Service
 public class LoginCRUDService implements ILoginCRUDService {
+    Firestore dbFirestore = FirestoreClient.getFirestore();
+
     @Override
     public String createLogin(Login login) throws ExecutionException, InterruptedException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("Logins").document(login.getUsername()).set(login);
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("Logins").document(login.getLoginId()).set(login);
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
 
     @Override
-    public Login getLogin(int loginId) {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
+    public Login getLogin(String loginId) throws ExecutionException, InterruptedException{
+        DocumentReference documentReference = dbFirestore.collection("Logins").document(loginId);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+        Login login;
+        if(document.exists()){
+            login = document.toObject(Login.class);
+            return login;
+        }
         return null;
     }
 
     @Override
-    public String updateLogin(Login login) {
-        return null;
+    public String updateLogin(Login login) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection("Logins").document(login.getLoginId()).set(login);
+        return collectionApiFuture.get().getUpdateTime().toString();
     }
 
     @Override
-    public String deleteLogin(int loginId) {
-        return null;
+    public String deleteLogin(String loginId) {
+        ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection("Logins").document(loginId).delete();
+        return "Successfully deleted " + loginId;
     }
 }
