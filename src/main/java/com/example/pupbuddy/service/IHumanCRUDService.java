6e3ca19e56f0f0b25487a26b430d271e56808a37
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-04-11 15:12:55 -0400

    IHumanCRUDService and HumanCRUDService

---
+++ src/main/java/com/example/pupbuddy/service/IHumanCRUDService.java
@@ -0,0 +1,12 @@
+package com.example.pupbuddy.service;
+
+import com.example.pupbuddy.dto.Human;
+
+import java.util.concurrent.ExecutionException;
+
+public interface IHumanCRUDService {
+    public String createHuman(Human human) throws ExecutionException, InterruptedException;
+    public Human getHuman(String humanId) throws ExecutionException, InterruptedException;
+    public String updateHuman(Human human) throws ExecutionException, InterruptedException;
+    public String deleteHuman(String humanId);
+}
