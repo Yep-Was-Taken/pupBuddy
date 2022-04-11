387471614ea946882aeeefe4b32d84d1fe862f91
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-04-11 15:13:50 -0400

    IChoreCRUDService ChoreCRUDService

---
+++ src/main/java/com/example/pupbuddy/service/ChoreCRUDService.java
@@ -0,0 +1,48 @@
+package com.example.pupbuddy.service;
+
+import com.example.pupbuddy.dto.Chore;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class ChoreCRUDService implements IChoreCRUDService{
+    Firestore dbFirestore = FirestoreClient.getFirestore();
+
+    @Override
+    public String createChore(Chore chore) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("Chores").document(chore.getChoreId()).set(chore);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    @Override
+    public Chore getChore(String choreId) throws ExecutionException, InterruptedException {
+        DocumentReference documentReference = dbFirestore.collection("Chores").document(choreId);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+        Chore chore;
+        if(document.exists()){
+            chore = document.toObject(Chore.class);
+            return chore;
+        }
+        return null;
+    }
+
+    @Override
+    public String updateChore(Chore chore) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection("Chores").document(chore.getChoreId()).set(chore);
+        return collectionApiFuture.get().getUpdateTime().toString();
+    }
+
+    @Override
+    public String deleteChore(String choreId) {
+        ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection("Chores").document(choreId).delete();
+        return "Successfully deleted " + choreId;
+    }
+}
---
+++ src/main/java/com/example/pupbuddy/service/IChoreCRUDService.java
@@ -0,0 +1,12 @@
+package com.example.pupbuddy.service;
+
+import com.example.pupbuddy.dto.Chore;
+
+import java.util.concurrent.ExecutionException;
+
+public interface IChoreCRUDService {
+    public String createChore(Chore chore) throws ExecutionException, InterruptedException;
+    public Chore getChore(String choreId) throws ExecutionException, InterruptedException;
+    public String updateChore(Chore chore) throws ExecutionException, InterruptedException;
+    public String deleteChore(String choreId);
+}
